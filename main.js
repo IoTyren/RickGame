241fb9e8621586a3f82d492591cbf7aac7e8167c
Leon Johnson <SlurtoGames@gmail.com>
2022-01-30 13:15:20 -0500

    Listeners and variables

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-alert("Broooo");
-alert("YOOOOO");
+let clickers = 50;
+let startTime = Date.now();
+
+// position element in DOM
+function sync (dom, pos) {
+    dom.style.left = `${pos.x}px`
+    dom.style.top = `${pos.y}px`
+
+}
+`${pos.x}px`
+function addClicker () {
+    const pos = {
+        x: Math.random() * 500,
+        y: Math.random() * 300
+    };
+    const img = new Image();
+    img.src = "rick.png";
+    img.style.position = "absolute";
+    img.addEventListener("click", removeClicker, false);
+
+    document.querySelector("#board").appendChild(img);
+    sync(img, pos);
+
+}
 
